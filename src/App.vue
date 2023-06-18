533bd7b2a5945dbb16eb4b959af831466bd940e4
juaamol <jambulud@gmail.com>
2023-06-18 21:19:11 +0200

    Split content in separate components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { db } from './data/instruments';
+import Instrument from './components/Instrument.vue';
+import Header from './components/Header.vue';
 
 const instruments = ref([]);
 
@@ -10,125 +12,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <header class="py-5 header">
-    <div class="container-xl">
-      <div class="row justify-content-center justify-content-md-between">
-        <div class="col-8 col-md-3">
-          <a href="index.html">
-            <div class="d-flex flex-column text-white">
-              <span class="fs-4"
-                >Guitar <span class="text-primary">Shop</span></span
-              >
-              <span class="fs-5">Instruments and courses</span>
-            </div>
-          </a>
-        </div>
-        <nav
-          class="col-md-6 a mt-5 d-flex align-items-start justify-content-end"
-        >
-          <div class="carrito">
-            <img
-              class="img-fluid"
-              src="/img/carrito.png"
-              alt="imagen carrito"
-            />
-
-            <div id="carrito" class="bg-white p-3">
-              <p class="text-center">Cart is empty</p>
-              <table class="w-100 table">
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <img
-                        class="img-fluid"
-                        src="/img/guitarra_02.jpg"
-                        alt="imagen guitarra"
-                      />
-                    </td>
-                    <td>SRV</td>
-                    <td class="fw-bold">$299</td>
-                    <td class="flex align-items-start gap-4">
-                      <button type="button" class="btn btn-dark">-</button>
-                      1
-                      <button type="button" class="btn btn-dark">+</button>
-                    </td>
-                    <td>
-                      <button class="btn btn-danger" type="button">X</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-
-              <p class="text-end">Total: <span class="fw-bold">$899</span></p>
-              <button class="btn btn-dark w-100 mt-3 p-2">Empty cart</button>
-            </div>
-          </div>
-        </nav>
-      </div>
-      <!--.row-->
-
-      <div class="row mt-5">
-        <div class="col-md-6 text-center text-md-start pt-5">
-          <h1 class="display-2 fw-bold">Modelo VAI</h1>
-          <p class="mt-5 fs-5 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-            possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias
-            optio tempore sint at ipsa dolorum odio exercitationem eos inventore
-            odit.
-          </p>
-          <p class="text-primary fs-1 fw-black">$399</p>
-          <button
-            type="button"
-            class="btn fs-4 bg-primary text-white py-2 px-5"
-          >
-            Add to cart
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <img
-      class="header-guitarra"
-      src="/img/header_guitarra.png"
-      alt="imagen header"
-    />
-  </header>
+  <Header></Header>
 
   <main class="container-xl mt-5">
     <h2 class="text-center">Our collection</h2>
 
     <div class="row mt-5">
-      <div
-        class="col-md-6 col-lg-4 my-4 row align-items-center"
+      <Instrument
         v-for="instrument in instruments"
-      >
-        <div class="col-4">
-          <img
-            class="img-fluid"
-            v-bind:src="`/img/${instrument.image}.jpg`"
-            alt="imagen guitarra"
-          />
-        </div>
-        <div class="col-8">
-          <h3 class="text-black fs-4 fw-bold text-uppercase">
-            {{ instrument.name }}
-          </h3>
-          <p>
-            {{ instrument.description }}
-          </p>
-          <p class="fw-black text-primary fs-3">${{ instrument.price }}</p>
-          <button type="button" class="btn btn-dark w-100">Add to cart</button>
-        </div>
-      </div>
+        :image="`/img/${instrument.image}.jpg`"
+        :name="instrument.name"
+        :description="instrument.description"
+        :price="instrument.price"
+      />
     </div>
   </main>
 
---
+++ src/components/Instrument.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img class="img-fluid" :src="image" alt="imagen guitarra" />
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">
+        {{ name }}
+      </h3>
+      <p>
+        {{ description }}
+      </p>
+      <p class="fw-black text-primary fs-3">${{ price }}</p>
+      <button type="button" class="btn btn-dark w-100">Add to cart</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
